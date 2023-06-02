ba87ee7f0bf3640c0a1ab9c2a2e4e5594edf2dc9
ASSiddik01 <assiddik001@gmail.com>
2023-06-02 17:42:27 +0600

    logger store on daily basis

--- src/utilities/logger.ts
+++ src/utilities/logger.ts
@@ -1,27 +1,53 @@
 /* eslint-disable no-undef */
-import winston from 'winston'
+import { createLogger, format, transports } from 'winston'
+const { combine, timestamp, label, printf } = format
+import DailyRotateFile from 'winston-daily-rotate-file'
 import path from 'path'
 
-export const logger = winston.createLogger({
+const myFormat = printf(({ level, message, label, timestamp }) => {
+  const date = new Date(timestamp)
+
+  return `${date} - [${label}] ${level}: ${message}`
+})
+
+export const logger = createLogger({
   level: 'info',
-  format: winston.format.json(),
+  format: combine(label({ label: 'UMS' }), timestamp(), myFormat),
   transports: [
-    new winston.transports.Console(),
-    new winston.transports.File({
-      filename: path.join(process.cwd(), 'logs', 'winston', 'success.log'),
-      level: 'info',
+    new transports.Console(),
+    new DailyRotateFile({
+      filename: path.join(
+        process.cwd(),
+        'logs',
+        'winston',
+        'successes',
+        'ums-%DATE%-success.log'
+      ),
+      datePattern: 'HH - DD.MM.YYYY',
+      zippedArchive: true,
+      maxSize: '20m',
+      maxFiles: '14d',
     }),
   ],
 })
 
-export const errorLogger = winston.createLogger({
+export const errorLogger = createLogger({
   level: 'error',
-  format: winston.format.json(),
+  format: combine(label({ label: 'UMS' }), timestamp(), myFormat),
   transports: [
-    new winston.transports.Console(),
-    new winston.transports.File({
-      filename: path.join(process.cwd(), 'logs', 'winston', 'error.log'),
-      level: 'error',
+    new transports.Console(),
+    new DailyRotateFile({
+      filename: path.join(
+        process.cwd(),
+        'logs',
+        'winston',
+        'errors',
+        'ums-%DATE%-error.log'
+      ),
+      datePattern: 'HH - DD.MM.YYYY',
+      zippedArchive: true,
+      maxSize: '20m',
+      maxFiles: '14d',
     }),
   ],
 })
