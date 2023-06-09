3e9f7f0f4193fab474f28d80a0689dbe3e43e33d
shama <assiddik001@gmail.com>
2023-06-09 14:26:42 +0600

    formation trycatch error and send response

--- src/app/modules/users/user.controller.ts
+++ src/app/modules/users/user.controller.ts
@@ -1,19 +1,18 @@
-import { RequestHandler } from 'express'
+import { NextFunction, Request, Response } from 'express'
 import { createUserService } from './user.services'
+import { sendRes } from '../../../utilities/sendRes'
+import { tryCatch } from '../../../utilities/tryCatch'
+import status from 'http-status'
 
-export const createUser: RequestHandler = async (req, res) => {
-  try {
+export const createUser = tryCatch(
+  async (req: Request, res: Response, next: NextFunction) => {
     const result = await createUserService(req.body)
-    res.status(200).send({
+    sendRes(res, {
+      statusCode: status.OK,
       success: true,
       message: 'Create users successfully',
       result: result,
     })
-  } catch (error) {
-    res.status(400).send({
-      success: false,
-      message: 'Create users failed',
-      result: error,
-    })
+    next()
   }
-}
+)
